8283542ec100e8af87e9f439f7669e49ef512dd5
Andy Yong <andyywz@gmail.com>
2023-09-09 16:55:05 -0400

    add auth specs

--- spec/system/authentication_spec.rb
+++ spec/system/authentication_spec.rb
@@ -13,7 +13,7 @@ describe "Authentication", type: :system do
   it "redirects users to the sign in page when visiting the new dance event page" do
     visit "/dance_events/new"
 
-    expect(page).to have_content("You need to sign in or sign up before continuing.")
+    expect(page).to have_content("You need to sign in or sign up before continuing")
     expect(page).to have_current_path("/users/sign_in")
   end
 
@@ -21,7 +21,66 @@ describe "Authentication", type: :system do
     dance_event = FactoryBot.build_stubbed(:dance_event)
     visit "/dance_events/#{dance_event.id}/edit"
 
-    expect(page).to have_content("You need to sign in or sign up before continuing.")
+    expect(page).to have_content("You need to sign in or sign up before continuing")
     expect(page).to have_current_path("/users/sign_in")
+  end
+
+  it "allows new users to register" do
+    visit "/"
+    click_on "Sign in"
+    click_on "Sign up"
+    fill_in "Email", with: "test@test.com"
+    fill_in "Username", with: "test"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_on "Sign up"
+
+    expect(page).to have_content("Welcome! You have signed up successfully")
+  end
+
+  it "allows users to login with username" do
+    user = FactoryBot.create(:user, username: 'test', email: 'test@test.com', password: 'password')
+
+    visit "/"
+    click_on "Sign in"
+    fill_in "Your email/username", with: user.username
+    fill_in "Your password", with: 'password'
+    click_on "Submit"
+
+    expect(page).to have_content("Signed in successfully")
+  end
+
+  it "allows users to login with email" do
+    user = FactoryBot.create(:user, username: 'test', email: 'test@test.com', password: 'password')
+
+    visit "/"
+    click_on "Sign in"
+    fill_in "Your email/username", with: user.email
+    fill_in "Your password", with: 'password'
+    click_on "Submit"
+
+    expect(page).to have_content("Signed in successfully")
+  end
+
+  it "displays an error when user enters the wrong credentials" do
+    user = FactoryBot.create(:user, username: 'test', email: 'test@test.com', password: 'password')
+
+    visit "/"
+    click_on "Sign in"
+    fill_in "Your email/username", with: user.username
+    fill_in "Your password", with: 'badpassword'
+    click_on "Submit"
+
+    expect(page).to have_content("Invalid Login or password")
+  end
+
+  it "does not indicate existing email or username" do
+    visit "/"
+    click_on "Sign in"
+    fill_in "Your email/username", with: 'randomusername'
+    fill_in "Your password", with: 'password'
+    click_on "Submit"
+
+    expect(page).to have_content("Invalid Login or password")
   end
 end
---
+++ spec/system/dance_events/creating_dance_events_spec.rb
@@ -0,0 +1,7 @@
+require "rails_helper"
+
+describe "Creating Dance Events" do
+  scenario 'user can create dance event' do
+    
+  end
+end
