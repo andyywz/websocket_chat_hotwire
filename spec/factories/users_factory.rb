81264660695abc9a6af8de2851623b57a8ff4be3
Andy Yong <andyywz@gmail.com>
2023-10-22 20:11:46 -0400

    fix test

--- spec/factories/users_factory.rb
+++ spec/factories/users_factory.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user, aliases: [:organizer] do
-    sequence(:email) { |n| "person#{n}@example.com" }
+    sequence(:email) { |n| "person_#{n}@example.com" }
     sequence(:username) { |n| "person#{n}" }
     password { "password" }
 
